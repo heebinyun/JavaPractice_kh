4dbc9d183596f2ee0cd43e84ca5a5ae6dd6927ce
heebinyun <heebin.dev@gmail.com>
2020-05-27 10:31:53 +0900

    200527 sort

--- javaPractice/src/array/ArraySort.java
+++ javaPractice/src/array/ArraySort.java
@@ -63,4 +63,25 @@ public class ArraySort {
 		}
 		System.out.println("count : " + count);
 	}
+	
+	public void method3() {
+		
+		int[] arr = {7, 1, 5, 3, 4, 2, 6};
+		
+		// 삽입 정렬 (오름차순)
+		for(int i=1; i<arr.length; i++) {
+			
+			for(int j=0; j<i; j++) {
+				
+				if(arr[i] < arr[j]) {
+					int temp = arr[i];
+						arr[i] = arr[j];
+						arr[j] = temp;
+				}
+			}
+		}
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
 }
